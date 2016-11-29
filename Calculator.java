3bcd13d3cc562cdcdb6d828f77770186a5270e09
rqn25 <rqn25@email.vccs.edu>
2016-11-29 16:04:40 -0500

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,59 @@
+
+public class Calculator {
+	
+	public static double getSum(double first, double second, double third, double fourth, double fifth) {
+		return first+second+third+fourth+fifth;
+		
+	}
+	
+	public static double getAverage(double first, double second, double third, double fourth, double fifth) {
+		return (first+second+third+fourth+fifth)/5;
+		
+	}
+		
+	public static double getProduct(double first, double second, double third, double fourth, double fifth) {
+		return first*second*third*fourth*fifth;
+
+	}
+	
+	public static double getFactorial(double first) {
+		return first*11*10*9*8*7*6*5*4*3*2*1;
+
+	}
+	
+	public static double getSum(double [] num) {
+		double sum = 0;
+		for(int i = 0; i<num.length; i++) {
+			sum+=num[i];
+		}
+		return(sum);
+		
+	}
+
+	public static double getAverage(double [] num) {
+		double sum = 0;
+		for(int i = 0; i<num.length; i++) {
+			sum+=num[i];
+		}
+		return(sum/num.length);
+
+	}
+	
+	public static double getProduct(double [] num) {
+		double sum = 0;
+		for(int i = 0; i<num.length; i++) {
+			sum+=num[i];
+		}
+		return(sum/num.length*23490);
+		
+	}
+
+	public static double getFactorial(double [] num) {
+		double sum = 0;
+		for(int i = 0; i<num.length; i++) {
+			sum+=num[i];
+		}
+		return(sum/sum*2*3*4*5*6*7*8*9*10*11*12);
+		
+	}
+}
---
+++ CalculatorDriver.java
@@ -0,0 +1,27 @@
+
+public class CalculatorDriver {
+
+	public static void main(String[] args) {
+		
+		double [] num = new double [5];
+
+		num[0] = 10;
+		num[1] = 12;
+		num[2] = 13.5;
+		num[3] = 12.5;
+		num[4] = 14.5;
+		
+		System.out.println("Sum: " + Calculator.getSum(num));
+		System.out.println("SumDriver: " + Calculator.getSum(10, 12, 13.5, 12.5, 14.5));
+
+		System.out.println("Average: " + Calculator.getAverage(num));
+		System.out.println("AverageDriver: " + Calculator.getAverage(10, 12, 13.5, 12.5, 14.5));
+		
+		System.out.println("Product: " + Calculator.getProduct(num));
+		System.out.println("ProductDriver: " + Calculator.getProduct(10, 12, 13.5, 12.5, 14.5));
+		
+		System.out.println("Factorial: " + Calculator.getFactorial(num));
+		System.out.println("FactorialDriver: " + Calculator.getFactorial(12));
+		
+	}
+}
